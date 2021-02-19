6806116fc2a6b1d47c0a09f13059dd7d9c9979e7
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2021-02-19 16:33:38 -0300

    Add base admin form structure

--- front-web/src/pages/Admin/index.tsx
+++ front-web/src/pages/Admin/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import { Switch, Route } from 'react-router-dom';
+import './styles.scss';
+import Products from './components/Products';
 
 const Admin = () =>  (
-  <div>
+  <div className="admin-container">
     <Navbar />
+    <div className="admin-content">
+      <Switch>
+        <Route path="/admin/products">
+          <Products />
+        </Route>
+        <Route path="/admin/categories">
+          <h1>Categories</h1>
+        </Route>
+        <Route path="/admin/users">
+          <h1>Users</h1>
+        </Route>
+      </Switch>
+    </div>
   </div>
 );
 
